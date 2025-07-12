50b4c293241fdfa215a7de6fe7b5297b2987fcb2
himanshu22 <himanshukumar_se22a11_61@dtu.ac.in>
2025-07-12 12:34:06 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import TypingBox from './components/TypingBox';
+import Timer from './components/Timer';
+import Stats from './components/Stats';
+import Settings from './components/Settings';
+import './App.css';
